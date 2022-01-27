182220d37604364bff3617565d086b7bfaa41f70
SimonDuarte <simoda18@uia.no>
2022-01-27 19:26:21 +0100

    myquote

---
+++ myquote.go
@@ -0,0 +1,21 @@
+package myquote
+    import (
+    "fmt"
+    "rsc.io/quote"
+    )
+
+    func Hello() {
+    fmt.Println(quote.Hello())
+    }
+
+    func Opt(){
+    fmt.Println(quote.Opt())
+    }
+
+    func Glass(){
+    fmt.Println(quote.Glass())
+    }
+
+    func Go(){
+        fmt.Println(quote.Go())
+        }
